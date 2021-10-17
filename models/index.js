f9e8ef079ec67c17cc8a633278eda4e1468cd0ec
lamperouge <philbohn1791@gmail.com>
2021-10-17 12:32:26 -0400

    added associations to the index.js of models

--- models/index.js
+++ models/index.js
@@ -10,4 +10,28 @@ Post.belongsTo(User, {
     foreignKey: "user_id"
 });
 
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE",
+    hooks: true,
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+    hooks: true,
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE",
+    hooks: true,
+});
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+    hooks: true,
+});
+
 module.exports = { User, Comment, Post}
