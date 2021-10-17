5b16cdd77211be782122f130ca3b9ee4f487ee6e
lamperouge <philbohn1791@gmail.com>
2021-10-17 14:32:31 -0400

    fixed some of the comments in controllers/api/post-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,9 +4,9 @@ const { User, Comment, Post} = require("../../models");
 // TODO: POST route to create new Comment, if logged in
 
 // TODO: GET route to get all COmments, possibly a comment by User ID, or by Comment ID
-    // This seems like it might not be necessary, though I am unsure at this time
 
 // TODO: UPDATE route to edit a Comment, if logged in
 
 // TODO: DELETE route to delete a Comment, if logged in
     // Comments can only be deleted by the user who made them??
+    // Functionality for this needs to be figured out
