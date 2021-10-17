d91d285c03c4415672e2c8a1091b7691656281b2
lamperouge <philbohn1791@gmail.com>
2021-10-17 12:36:25 -0400

    reformatted models/index.js to be more aesthetically pleasing

--- models/index.js
+++ models/index.js
@@ -2,33 +2,41 @@ const User = require("./User");
 const Comment = require("./Comment");
 const Post = require("./Post");
 
+// Users can have many posts associated with them
 User.hasMany(Post, {
     foreignKey: "user_id"
 });
 
+// Reciprocal association of above: Many posts can belong to one user
 Post.belongsTo(User, {
     foreignKey: "user_id"
 });
 
-Comment.belongsTo(Post, {
-    foreignKey: "post_id",
-    onDelete: "CASCADE",
-    hooks: true,
-});
-
-Comment.belongsTo(User, {
-    foreignKey: "user_id",
-    onDelete: "CASCADE",
-    hooks: true,
-});
-
+// Posts can have many comments associated with them.
+// When a post is deleted, all of its comments are as well
 Post.hasMany(Comment, {
     foreignKey: "post_id",
     onDelete: "CASCADE",
     hooks: true,
 });
 
+// Reciprocal association of above
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE",
+    hooks: true,
+});
+
+// Users can have many comments associated with them.
+// When a user is deleted, all of its comments are as well
 User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+    hooks: true,
+});
+
+// Reciprocal association of above
+Comment.belongsTo(User, {
     foreignKey: "user_id",
     onDelete: "CASCADE",
     hooks: true,
