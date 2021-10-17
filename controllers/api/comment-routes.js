89080dedfd1bd83e4baad3cb31f0116fc46f80aa
lamperouge <philbohn1791@gmail.com>
2021-10-17 14:30:11 -0400

    added routes to controllers/api/index.js and added comments on functionality and routes to comment- and post-routes.js

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { User, Comment, Post} = require("../../models");
+
+// TODO: POST route to create new Comment, if logged in
+
+// TODO: GET route to get all COmments, possibly a comment by User ID, or by Comment ID
+    // This seems like it might not be necessary, though I am unsure at this time
+
+// TODO: UPDATE route to edit a Comment, if logged in
+
+// TODO: DELETE route to delete a Comment, if logged in
+    // Comments can only be deleted by the user who made them??
---
+++ controllers/api/post-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { User, Comment, Post} = require("../../models");
+
+// TODO: POST route to create new Post, if logged in
+
+// TODO: GET route to get all Posts, possibly a post by User ID, or by Post ID
+
+// TODO: UPDATE route to edit a Post, if logged in
+
+// TODO: DELETE route to delete a Post, if logged in
