b38a27eb4ef2fcd4e8db0f1c7d0935636c173fc4
lamperouge <philbohn1791@gmail.com>
2021-10-18 16:35:08 -0400

    got the server.js page set up properly. Beginning work on routes so that handlebar can populate properly

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 const path = require('path');
 const express = require('express');
-// Import express-session
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
-const { Sequelize } = require('sequelize/types');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up sessions
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    // Stored in milliseconds (86400 === 1 day)
-    maxAge: 86400,
-  },
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
-  }),
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
-
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
