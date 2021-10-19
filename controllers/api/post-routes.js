3b145cc2db855942ddb248a583c866b998a6f266
lamperouge <philbohn1791@gmail.com>
2021-10-19 16:35:59 -0400

    post routes now hooked up to server

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,7 +6,7 @@ const withAuth = require("../../utils/auth");
 router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
-      attributes: ["id", "title", "post_text"],
+      attributes: ["id", "title", "post_text", "created_at"],
     });
     res.status(200).json(postData);
   } catch (err) {
@@ -31,3 +31,5 @@ router.post("/", withAuth, async (req, res) => {
 // TODO: UPDATE route to edit a Post, if logged in
 
 // TODO: DELETE route to delete a Post, if logged in
+
+module.exports = router;
