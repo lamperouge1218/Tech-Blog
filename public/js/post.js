34db92f2a50b94f6e3fde06c686eb2ba64baffb4
lamperouge <philbohn1791@gmail.com>
2021-10-19 18:05:41 -0400

    still fiddling with post routes and rendering things to the dashboard page. unsuccessful on all accounts

--- public/js/post.js
+++ public/js/post.js
@@ -1,13 +1,14 @@
 async function postFormHandler(event) {
   event.preventDefault();
+  console.log("hello world");
   // get the information from the post form
-  const title = document.querySelector("#post-title").value.trim();
-  const post_text = document.querySelector("#post-text").value.trim();
+  const title = document.querySelector("#post-title").value;
+  const post_text = document.querySelector("#post-text").value;
 
   // if both fields have content
   if (title && post_text) {
     // POST to the post route
-    const response = await fetch("/api/post", {
+    const response = await fetch("/api/posts", {
       method: "POST",
       body: JSON.stringify({
         title,
