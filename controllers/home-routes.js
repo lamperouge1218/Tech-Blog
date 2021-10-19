5b554b4518fa2f3566fbd8aeff127af22ba2f5ac
lamperouge <philbohn1791@gmail.com>
2021-10-18 20:08:54 -0400

    homepage is creating comment cards as intended!

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
 const { User, Comment, Post} = require('../models');
 
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"]
+        },
+      ],
+    });
+    const posts = postData.map((userPosts) => userPosts.get({plain: true}));
+
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Login route
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect to the homepage
--- seeds/postData.js
+++ seeds/postData.js
@@ -5,24 +5,28 @@ const postdata = [
     {
         title: "I am a computer fan",
         post_text: "Just love these cool computers!",
+        date: "10/1/2021",
         user_id: "2",
     },
     // Post ID 2
     {
         title: "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like? ",
         post_text: "Have they?",
+        date: "10/1/2021",
         user_id: "1",
     },
     // Post ID 3
     {
         title: "According to all known laws of aviation...",
         post_text: "there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways. Because bees don't care what humans think is impossible.",
+        date: "10/1/2021",
         user_id: "1",
     },
     // Post ID 4
     {
         title: "Somebody...",
         post_text: "once told me the world is gonna roll me, I aint the sharpest tool in the shed. She was looking kind of dumb with her finger and her thumb In the shape of an L on her forehead.",
+        date: "10/1/2021",
         user_id: "3",
     },
 ];
