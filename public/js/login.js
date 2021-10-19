52e786ddeda9f63b7269473acf18499703956ba9
lamperouge <philbohn1791@gmail.com>
2021-10-19 09:46:40 -0400

    logout function working properly:

--- public/js/login.js
+++ public/js/login.js
@@ -50,6 +50,6 @@ document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
 
-// document
-//   .querySelector(".signup-form")
-//   .addEventListener("submit", signupFormHandler);
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
