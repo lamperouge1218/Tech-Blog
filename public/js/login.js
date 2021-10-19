63e11d972c58121e50c56a32225d417df6239b30
lamperouge <philbohn1791@gmail.com>
2021-10-19 13:57:17 -0400

    got proper id for queryselector on line 30 of login.js

--- public/js/login.js
+++ public/js/login.js
@@ -27,7 +27,7 @@ async function loginFormHandler(event) {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector("#name-signup").value.trim();
+  const name = document.querySelector("#username-signup").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
